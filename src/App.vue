62f2833f0327ae7e2e10ae7e74bba9e29827b982
Maureen Chang <tz14pon@gmail.com>
2022-01-12 22:51:02 +0800

    fetch data & start working on product component

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <Product v-for="(item,index) in dealingList" :key="'dealing'+index" :item="item"/>
+    <Product v-for="(item,index) in completeList" :key="'complete'+index" :item="item"/>
+  </div>
+</template>
+
+<script>
+import Product from './components/Product'
+
+export default {
+  name: 'App',
+  components: {
+    Product
+  },
+  data(){
+    return {
+      productList: null,
+    }
+  },
+  created(){
+    this.load()
+  },
+  methods: {
+    async load(){
+      try {
+        let data = await fetch('http://localhost:3000/orders')
+        if(!data.ok){
+          throw Error('no data available')
+        }
+        this.productList = await data.json()
+      } catch (err) {
+        console.log(err.message)
+      }
+    },
+  },
+  computed: {
+    dealingList(){
+      if(!this.productList) return
+      const arr = this.productList
+      .filter(item => item.status.code === 1 || item.status.code === 2)
+      arr.map((item)=>{
+        let date = item.date.split('/')
+        date[0] = date[0]*1+1911+''
+        item.dateMilliseconds = Date.parse(date.join('-'))
+      })
+      arr.sort((a, b) => b.dateMilliseconds-a.dateMilliseconds)
+      return arr
+    },
+    completeList(){
+      if(!this.productList) return
+      const arr = this.productList.filter(item => item.status.code === 3 || item.status.code === 4)
+      arr.map((item)=>{
+        let date = item.date.split('/')
+        date[0] = date[0]*1+1911+''
+        console.log(date)
+        item.dateMilliseconds = Date.parse(date.join('-'))
+      })
+      arr.sort((a, b) => b.dateMilliseconds-a.dateMilliseconds)
+      return arr
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product">
+    <div>
+      <img :src="item.logo" alt="logo" width="312" height="312" />
+    </div>
+    <div>
+      <time>{{item.date}}</time>
+      <p>{{item.status.type}}</p>
+      <h4>{{item.name}}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props:{
+    item: Object,
+    required: true,
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
